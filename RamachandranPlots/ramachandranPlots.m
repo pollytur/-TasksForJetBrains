2f703e6f7fb8f534be3836fd85453456e5980fa0
PollyTur <49938348+pollytur@users.noreply.github.com>
2020-04-02 21:56:11 +0300

    added solution in Matlab

---
+++ RamachandranPlots/ramachandranPlots.m
@@ -0,0 +1,6 @@
+Struct1a22 = getpdb('1a22'); 
+ramachandran(Struct1a22,'glycine',true,'regions',true); 
+Struct6SRY = getpdb('6SRY'); 
+ramachandran(Struc6SRY,'glycine',true,'regions',true); 
+Struct1E7I = getpdb('1E7I');  
+ramachandran(Struct1E7I,'glycine',true,'regions',true); 
